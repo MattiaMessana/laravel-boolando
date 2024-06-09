7b92b942cdca2996501b725cf5aabaf167cd1b72
MattiaMessana <mattia.messana@icloud.com>
2024-06-09 21:34:30 +0200

    add:cards + hover effect

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,7 +1,9 @@
-<div class="ms_card">
-    <img src="{{ Vite::asset('resources/img' . $prod['frontImage']) }}" alt="">
-    <div class="ms_card-content">
-        <p>{{ $prod['brand']}}</p>
-        <h4 class="text-center">{{ $prod['name']}}</h4>
+<div class="ms_card mt-4">
+    <img class="w-100 front" src="{{ Vite::asset('resources/img/' . $prod['frontImage']) }}" alt="">
+    <img class="w-100 back" src="{{ Vite::asset('resources/img/' . $prod['backImage']) }}" alt="">
+    <div class="ms_card-content p-2">
+        <p>{{ $prod['brand'] }}</p>
+        <h5>{{ $prod['name'] }}</h5>
+        <p>{{ $prod['price'] }} €</p>
     </div>
 </div>
