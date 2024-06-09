d3cb910b9cc0b5269d6bdc2026bbcd58ec50b1b7
MattiaMessana <mattia.messana@icloud.com>
2024-06-09 22:32:22 +0200

    add: z-index header

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,9 +1,9 @@
-<div class="ms_card mt-4">
+<div class="ms_card mt-4 mb-4">
     <img class="w-100 front" src="{{ Vite::asset('resources/img/' . $prod['frontImage']) }}" alt="">
     <img class="w-100 back" src="{{ Vite::asset('resources/img/' . $prod['backImage']) }}" alt="">
     <div class="ms_card-content p-2">
-        <p>{{ $prod['brand'] }}</p>
+        <p class="text-muted">{{ $prod['brand'] }}</p>
         <h5>{{ $prod['name'] }}</h5>
-        <p>{{ $prod['price'] }} €</p>
+        <p class="text-muted d-inline">{{ $prod['price'] }} €</p>
     </div>
 </div>
